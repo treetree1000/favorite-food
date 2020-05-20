d737d92d7aa928f0841948cf5fa47e6cdd9ac1dc
kobayashichihiro <kobayashichihiro@192.168.10.111>
2020-05-20 23:50:59 +0900

    hello.

---
+++ sw.kadai1/src/sw/kadai1/FavoriteFood.java
@@ -0,0 +1,13 @@
+package sw.kadai1;
+
+public class FavoriteFood {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("私の好きな食べ物はプリンです。");
+		System.out.println("なぜなら、甘くて美味しいからです。");
+
+	}
+
+}
